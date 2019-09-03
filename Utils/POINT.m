8bc91f7201917c71d0dbb9204a0f7d888921a31a
txdtplus <47321866+txdtplus@users.noreply.github.com>
2019-09-03 15:11:14 +0800

    Update POINT.m

--- Utils/POINT.m
+++ Utils/POINT.m
@@ -38,7 +38,7 @@ classdef POINT < handle
     end
     
     methods
-        function obj = POINT(geoloc,RPC,real_loc)
+        function obj = POINT(geoloc,RPC)
             % geoloc should be a [m,3] matrix.
             %   geoloc(:,1) is the longitude of GCPs.
             %   geoloc(:,2) is the latitude of GCPs.
@@ -55,17 +55,10 @@ classdef POINT < handle
             obj.Yn = (obj.Y-RPC.LAT_OFF)/RPC.LAT_SCALE;
             obj.Zn = (obj.Z-RPC.H_OFF)/RPC.H_SCALE;
             
-            if nargin == 3
-                obj.r = real_loc(:,1);
-                obj.c = real_loc(:,2);
-                obj.rn = (obj.r-RPC.LINE_OFF)/RPC.LINE_SCALE;
-                obj.cn = (obj.c-RPC.SAMP_OFF)/RPC.SAMP_SCALE;
-            else
-                obj.r = zeros(obj.m,1);
-                obj.c = zeros(obj.m,1);
-                obj.rn = zeros(obj.m,1);
-                obj.cn = zeros(obj.m,1);
-            end
+            obj.r = zeros(obj.m,1);
+            obj.c = zeros(obj.m,1);
+            obj.rn = zeros(obj.m,1);
+            obj.cn = zeros(obj.m,1);
             
             obj.u = zeros(obj.m,20);
             obj.u(:,1) = ones(obj.m,1);             obj.u(:,2) = obj.Xn;
@@ -104,10 +97,10 @@ classdef POINT < handle
             end
         end
         
-         function update_rc(obj,new_rc)
-             obj.r = new_rc(:,1);
-             obj.c = new_rc(:,2);
-         end
+        function update_rc(obj,new_rc)
+            obj.r = new_rc(:,1);
+            obj.c = new_rc(:,2);
+        end
     end
 end
 
