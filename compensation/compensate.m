d85dd50b486b8f8d8903ad3ecb147d50c70d360b
txdtplus <47321866+txdtplus@users.noreply.github.com>
2019-09-04 15:36:44 +0800

    update

--- compensation/compensate.m
+++
@@ -1,9 +0,0 @@
-function after_compen_loc = compensate(A,coff,cal_loc)
-
-L_ = A*coff;
-compen_loc = zeros(size(cal_loc));
-compen_loc(:,1) = L_(1:2:end-1);
-compen_loc(:,2) = L_(2:2:end);
-after_compen_loc = cal_loc + compen_loc;
-end
-
--- install.m
+++
@@ -1,7 +0,0 @@
-addpath('data')
-addpath('LSA_fun')
-addpath('read_fun')
-addpath('RPC_fun')
-addpath('Utils')
-addpath('compensation')
-addpath('gen_vgcp')
