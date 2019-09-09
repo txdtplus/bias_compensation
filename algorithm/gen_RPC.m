66fe45b9d41ac1151dca266c2b0966de0b2284b5
txdtplus <47321866+txdtplus@users.noreply.github.com>
2019-09-09 17:37:30 +0800

    update

--- algorithm/gen_RPC.m
+++ algorithm/gen_RPC.m
@@ -1,6 +1,6 @@
 function DRPC = gen_RPC(vgcp)
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
+%generate new RPC using gcps
+%   vgcp is vitual gcp
 m = vgcp.m;
 R = vgcp.rn;    
 C = vgcp.cn;
--- main.m
+++ main.m
@@ -5,9 +5,9 @@ addpath('algorithm')
 addpath('Utils')
 
 %% read RPC and GCP
-trainnum = 5;
-[geoloc,real_loc,GCPnum] = readGCP('GCP_IKONOS_bgrn.xlsx');
-[DRPC,Normalize_par] = readrpc('RPC_IKONOS_bgrn.txt','txt');
+trainnum = 30;
+[geoloc,real_loc,GCPnum] = readGCP('GCP2.xlsx');
+[DRPC,Normalize_par] = readrpc('RPC2.xml','xml');
 
 %% train and check points
 geoloc_train = geoloc(1:trainnum,:);
@@ -15,6 +15,9 @@ real_loc_train = real_loc(1:trainnum,:);
 
 geoloc_check = geoloc(trainnum+1:end,:);
 real_loc_check = real_loc(trainnum+1:end,:);
+
+% geoloc_check = geoloc(1:trainnum,:);
+% real_loc_check = real_loc(1:trainnum,:);
 
 %% generate rpc and gcp object
 rpc1 = RPC(DRPC,Normalize_par);
@@ -33,24 +36,21 @@ A0 = gcps.gen_A();
 after_compen_loc = compensate(A0,coff0,cal_loc);
 delta_loc2 =  real_loc_train - after_compen_loc;      % calculate error after compensation
 
-% %% generate vgcps(virtual ground control points)
-% N = 150;                                        % number of vgcps
-% imax = 9;
-% % vZn = (randi(9,N,1)-5)/5';
-% vZn = (1.6*rand(1,N)-1)';
-% vgcp = gen_vgcp(N,rpc1,coff0,gcps,vZn);
-% 
-% %% caculate new RPC cofficients
-% DRPC_new = gen_RPC(vgcp);
-% rpc2 = RPC(DRPC_new,Normalize_par);
-% 
-% %% write new RPC file
-% fwriteRPC('./data/new_RPC.txt',rpc2);
-% 
-% %% check points
-% ckp = POINT(geoloc_check,rpc2);
-% cal_loc2 = rpc2.obj2img(ckp);
-% delta_loc3 = real_loc_check - cal_loc2;            % calculate error before compensation
+%% generate vgcps(virtual ground control points)
+N = 10;                                        % sqrt of number of vgcps
+vgcp = gen_vgcp(N,rpc1,coff0,gcps,1);
+
+%% caculate new RPC cofficients
+DRPC_new = gen_RPC(vgcp);
+rpc2 = RPC(DRPC_new,Normalize_par);
+
+%% write new RPC file
+fwriteRPC('./data/new_RPC.txt',rpc2);
+
+%% check points
+ckp = POINT(geoloc_check,rpc2);
+cal_loc2 = rpc2.obj2img(ckp);
+delta_loc3 = real_loc_check - cal_loc2;            % calculate error before compensation
 
 
 
